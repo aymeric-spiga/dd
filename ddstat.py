dfa965afe2622c0d100eec05f89ed64ed0046a08
Aymeric Spiga <aymeric.spiga@lmd.jussieu.fr>
2015-10-23 18:45:40 +0200

    added limtime to statdd plots. added an attempt to plot fric velocity if available.

--- ddstat.py
+++ ddstat.py
@@ -3,11 +3,11 @@ import ppplot
 import numpy as np
 
 ### STATDD
-def statdd(namefile):
+def statdd(namefile,limtime=None):
 
     # load data
     data = np.loadtxt(namefile,delimiter=";")
-    t = data[:,0] ; n = data[:,1] ; s = data[:,2] ; d = data[:,3]
+    t = data[:,0] ; n = data[:,1] ; s = data[:,2] ; d = data[:,3] 
     
     # remove size and drop point when no vortex detected
     d[np.where(n==0)] = np.nan ; s[np.where(n==0)] = np.nan
@@ -21,6 +21,7 @@ def statdd(namefile):
     number.color = 'b'
     number.xlabel = "Local time (hour)"
     number.ylabel = "Detected vortices"
+    number.xmax = limtime
     number.makeshow()
     
     drop = ppplot.plot1d()
@@ -32,6 +33,7 @@ def statdd(namefile):
     drop.fmt = "%.1f"
     drop.xlabel = "Local time (hour)"
     drop.ylabel = "Maximum drop of detected vortices (Pa)"
+    drop.xmax = limtime
     drop.makeshow()
     
     size = ppplot.plot1d()
@@ -42,4 +44,22 @@ def statdd(namefile):
     size.color = 'g'
     size.xlabel = "Local time (hour)"
     size.ylabel = "Maximum size of detected vortices (m)"
+    size.xmax = limtime
     size.makeshow()
+
+    try:
+      w = data[:,4]
+      w[np.where(n==0)] = np.nan
+      fric = ppplot.plot1d()
+      fric.f = w
+      fric.x = t
+      fric.fmt = "%.2f"
+      fric.linestyle = ''
+      fric.marker = '.'
+      fric.color = 'm'
+      fric.xlabel = "Local time (hour)"
+      fric.ylabel = "Maximum friction velocity within a vortex (m/s)"
+      fric.xmax = limtime
+      fric.makeshow()
+    except:
+      pass
