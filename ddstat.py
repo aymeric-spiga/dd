cca9094bc0048b97d5144875ea6ccd6d9c591096
Aymeric Spiga <aymeric.spiga@upmc.fr>
2020-04-07 01:24:59 +0200

    a slightly more flexible ddstat

--- ddstat.py
+++ ddstat.py
@@ -6,7 +6,7 @@ import numpy as np
 def statdd(namefile,limtime=None):
 
     # load data
-    data = np.loadtxt(namefile,delimiter=";")
+    data = np.loadtxt(namefile+".txt",delimiter=";")
     t = data[:,0] ; n = data[:,1] ; s = data[:,2] ; d = data[:,3] 
     
     # remove size and drop point when no vortex detected
@@ -22,7 +22,9 @@ def statdd(namefile,limtime=None):
     number.xlabel = "Local time (hour)"
     number.ylabel = "Detected vortices"
     number.xmax = limtime
-    number.makeshow()
+    #number.makeshow()
+    number.make()
+    ppplot.save(mode="pdf",filename=namefile+"_detect")
     
     drop = ppplot.plot1d()
     drop.f = d
@@ -34,7 +36,9 @@ def statdd(namefile,limtime=None):
     drop.xlabel = "Local time (hour)"
     drop.ylabel = "Maximum drop of detected vortices (Pa)"
     drop.xmax = limtime
-    drop.makeshow()
+    #drop.makeshow()
+    drop.make()
+    ppplot.save(mode="pdf",filename=namefile+"_maxdrop")
     
     size = ppplot.plot1d()
     size.f = s
@@ -45,21 +49,22 @@ def statdd(namefile,limtime=None):
     size.xlabel = "Local time (hour)"
     size.ylabel = "Maximum size of detected vortices (m)"
     size.xmax = limtime
-    size.makeshow()
+    #size.makeshow()
+    ppplot.save(mode="pdf",filename=namefile+"_maxsize")
 
-    try:
-      w = data[:,4]
-      w[np.where(n==0)] = np.nan
-      fric = ppplot.plot1d()
-      fric.f = w
-      fric.x = t
-      fric.fmt = "%.2f"
-      fric.linestyle = ''
-      fric.marker = '.'
-      fric.color = 'm'
-      fric.xlabel = "Local time (hour)"
-      fric.ylabel = "Maximum friction velocity within a vortex (m/s)"
-      fric.xmax = limtime
-      fric.makeshow()
-    except:
-      pass
+#    try:
+#      w = data[:,4]
+#      w[np.where(n==0)] = np.nan
+#      fric = ppplot.plot1d()
+#      fric.f = w
+#      fric.x = t
+#      fric.fmt = "%.2f"
+#      fric.linestyle = ''
+#      fric.marker = '.'
+#      fric.color = 'm'
+#      fric.xlabel = "Local time (hour)"
+#      fric.ylabel = "Maximum friction velocity within a vortex (m/s)"
+#      fric.xmax = limtime
+#      fric.makeshow()
+#    except:
+#      pass
